f5f762db66c831ced0169ecfd701ae22f967d51d
Reid Netterville III <reidiiius@gmail.com>
2015-03-28 09:13:31 -0500

    150328.0

--- zaiyu.go
+++ zaiyu.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	var f float64
+
+	zhu.Zaiyu[0] = 1
+
 	for i := 0; i < 97; i++ {
 		f += 1
-		zhu.Zaiyu = append(zhu.Zaiyu, zhu.Semitone(f))
+		zhu.Zaiyu = append(zhu.Zaiyu, zhu.Microtone(f))
 	}
+
 	for i := 0; i < 97; i++ {
 		fmt.Println(zhu.Zaiyu[i])
 	}
--- zhu/zhu.go
+++ zhu/zhu.go
@@ -7,3 +7,7 @@ var Zaiyu = make([]float64, 1)
 func Semitone(n float64) float64 {
 	return math.Pow(2, n/12) * 1
 }
+
+func Microtone(n float64) float64 {
+	return math.Pow(2, n/24) * 1
+}
