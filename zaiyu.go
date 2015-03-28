b321b5f097e61822db5e1b78798bf968dc4ccfbc
Reid Netterville III <reidiiius@gmail.com>
2015-03-28 10:29:49 -0500

    150328.1

--- zaiyu.go
+++ zaiyu.go
@@ -10,12 +10,12 @@ func main() {
 
 	zhu.Zaiyu[0] = 1
 
-	for i := 0; i < 97; i++ {
+	for i := 0; i < 425; i++ {
 		f += 1
-		zhu.Zaiyu = append(zhu.Zaiyu, zhu.Microtone(f))
+		zhu.Zaiyu = append(zhu.Zaiyu, zhu.ChingFang(f))
 	}
 
-	for i := 0; i < 97; i++ {
+	for i := 0; i < 425; i++ {
 		fmt.Println(zhu.Zaiyu[i])
 	}
 }
--- zhu/zhu.go
+++ zhu/zhu.go
@@ -8,6 +8,14 @@ func Semitone(n float64) float64 {
 	return math.Pow(2, n/12) * 1
 }
 
+func Guillaume(n float64) float64 {
+	return math.Pow(2, n/19) * 1
+}
+
 func Microtone(n float64) float64 {
 	return math.Pow(2, n/24) * 1
+}
+
+func ChingFang(n float64) float64 {
+	return math.Pow(2, n/53) * 1
 }
